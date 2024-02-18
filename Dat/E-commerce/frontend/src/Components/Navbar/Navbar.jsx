affccf89a88dd5bfb08bda28b2ef1a65ba37ab43
TestingInUIT <21521951@gm.uit.edu.vn>
2024-02-18 21:04:32 +0700

    Cart-counting

--- Dat/E-commerce/frontend/src/Components/Navbar/Navbar.jsx
+++ Dat/E-commerce/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+
 export const Navbar = () => {
     const [menu,setMenu] = useState("shop"); // This is a state variable to keep track of the current menu item - use for nav-menu
+    const {getTotalCartItems} = useContext(ShopContext); // This is a state variable to keep track of the total number of items in the cart - use for nav-cart-count
+    const menuRef = useRef();
+
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -21,7 +26,7 @@ export const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
   )
