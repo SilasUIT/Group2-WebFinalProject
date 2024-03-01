977ec804771f9aa3218ec04a966dd2f3ac78f638
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-03-01 08:57:59 +0700

    update admin,connect test database,

---
+++ eassybooking-Admin/src/routes/admin/news/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const newController = require("../../../controllers/admin/news.controller");
+router.use(express.json());
+
+router.get("/form", newController.getForm);
+router.post(
+  "/form",
+  newController.addOrUpdateItem
+);
+router.get("/form/:id", newController.getForm);
+router.get("/delete/:id", newController.deleteItem);
+router.get('/changeStatus/:id/:status', newController.updateStatus);
+
+router.get("(/:status)?", newController.getAll);  
+
+router.get('(/:status)?',newController.statusCount);
+
+router.post("/changeStatusTool", newController.statusTool);
+router.post("/upload/:id", newController.imageUpload);
+module.exports = router;
