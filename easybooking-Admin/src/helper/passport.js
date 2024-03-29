5d205160ddd79c87962406acc2ef8c342bd0ed9f
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-03-29 21:33:32 +0700

    config admin, refix all

--- easybooking-Admin/src/helper/passport.js
+++ easybooking-Admin/src/helper/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../model/user.model');
+//const User = require('../model/user.model');
 
 passport.use(new LocalStrategy(
   {
--- easybooking-Admin/src/routes/admin/contact/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../../../controllers/admin/contact.controller')
-router.get('/form/:id' , contactController.getForm);
-router.get('', contactController.getAll);
-module.exports = router;
