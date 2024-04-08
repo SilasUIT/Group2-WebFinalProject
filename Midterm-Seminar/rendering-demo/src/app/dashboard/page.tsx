462d66937b4791d5f12202a6806387a995728208
TestingInUIT <21521951@gm.uit.edu.vn>
2024-04-08 23:39:07 +0700

    adding some page to mid-term seminar

--- Midterm-Seminar/rendering-demo/src/app/dashboard/page.tsx
+++ Midterm-Seminar/rendering-demo/src/app/dashboard/page.tsx
@@ -6,10 +6,14 @@ export default function DashboardPage() {
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <h1>Dashboard page</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <p>Hello, {name}!</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
+      <h1 className="text-2xl font-bold mb-4">Dashboard Page</h1>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+      />
+      <p className="text-xl">Hello, {name}!</p>
     </div>
   );
 }
