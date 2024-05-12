bd2896559a38a86651ddb27c45d57f505a97dac1
SilasUIT <155501706+SilasUIT@users.noreply.github.com>
2024-05-12 16:36:30 +0700

    tuoi

--- easybooking-Admin/public/product/js/custom.js
+++ easybooking-Admin/public/product/js/custom.js
@@ -34,13 +34,13 @@ const previewArrayimg = (input, name) => {
 };
 const handleDelete = (link) => {
 	Swal.fire({
-	  title: "Are you sure?",
-	  text: "You won't be able to revert this!",
-	  icon: "warning",
-	  showCancelButton: true,
-	  confirmButtonColor: "#3085d6",
-	  cancelButtonColor: "#d33",
-	  confirmButtonText: "Yes, delete it!",
+		title: "Bạn có chắc chắn muốn xóa không?",
+		text: "Bạn sẽ không thể khôi phục dữ liệu đã xóa!",
+		icon: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "Vâng, xóa đi!",
 	}).then((result) => {
 	  if (result.isConfirmed) {
 		document.location.href = link;
