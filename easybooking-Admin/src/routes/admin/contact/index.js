74c8e01cd0b4cb047c783e1615f0af7f781af9c2
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-05-19 12:19:04 +0700

    custom route

--- easybooking-Admin/src/routes/admin/contact/index.js
+++ easybooking-Admin/src/routes/admin/contact/index.js
@@ -5,4 +5,5 @@ const router = express.Router();
 const {catchAsync}=require('../../../apps/utils/catchAsync');
 
 router.get('/', catchAsync(contactController.getAll));
+router.get('/send',catchAsync(contactController.getForm));
 module.exports = router;
