d7e333fac09e6be617b4775535f5b9ba79143873
SilasUIT <155501706+SilasUIT@users.noreply.github.com>
2024-05-19 13:02:52 +0700

    add sent page

--- easybooking-Admin/src/routes/admin/contact/index.js
+++ easybooking-Admin/src/routes/admin/contact/index.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const {catchAsync}=require('../../../apps/utils/catchAsync');
 
 router.get('/', catchAsync(contactController.getAll));
-router.get('/send',catchAsync(contactController.getForm));
+router.get('/sent',catchAsync(contactController.getForm));
 module.exports = router;
