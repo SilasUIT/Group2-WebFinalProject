9d338b0e85c07c77db1789be40dab3e6c23d3808
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-05-22 12:24:19 +0700

    pull config getproduct setting

--- easybooking-Admin/src/services/booking.service.js
+++ easybooking-Admin/src/services/booking.service.js
@@ -2,7 +2,7 @@ const productmodel=require('../model/booking/product.model');
 
 class productservice{
     async getproducts(){
-        return await productmodel.find({status: "active",hireState:"rentOut"});
+        return await productmodel.find({status: "active"});
     }
 }
 
