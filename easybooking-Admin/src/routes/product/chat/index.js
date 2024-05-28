652cf54c1b8e8da410af348a7700928cdd6797db
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-05-28 15:23:22 +0700

    config socket, model storage --- none complete

--- easybooking-Admin/src/routes/product/chat/index.js
+++ easybooking-Admin/src/routes/product/chat/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 const {catchAsync}=require('../../../apps/utils/catchAsync');
 
 router.get('/', catchAsync(chatController.getAll));
+// router.get('/:id', catchAsync(chatController.getMessage));
+// router.post('/',catchAsync(chatController.createMessage));
+// router.get('/delete/:id',catchAsync(chatController.deleteMessage));
 module.exports = router;
