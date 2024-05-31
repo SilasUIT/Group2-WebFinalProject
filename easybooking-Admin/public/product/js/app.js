d2236d683523fa5a19529c8621c7884e3ee9b74a
SilasUIT <155501706+SilasUIT@users.noreply.github.com>
2024-05-31 15:43:27 +0700

    fix front-end shop.html

--- easybooking-Admin/public/product/js/app.js
+++ easybooking-Admin/public/product/js/app.js
@@ -101,3 +101,35 @@ document.querySelector('#next-year').onclick = () => {
     ++curr_year.value
     generateCalendar(curr_month.value, curr_year.value)
 }
+document.querySelector('#start-date-input').value = startDate;
+document.querySelector('#end-date-input').value = endDate;
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+    let startDate = selectionStart ? selectionStart.textContent : '';
+    let endDate = selectionEnd ? selectionEnd.textContent : '';
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+const gdate = new Date().toISOString().split('T')[0];
+document.getElementById("gdate").innerHTML = gdate;
